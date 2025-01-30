ad028a81b75d189e197d981ea03c5453acbfc603
zadmin <zadmin@vm-lab72-3-430.asia-south1-b.c.niit-stackroute-2.internal>
2025-01-30 16:21:07 +0530

    New commit

---
+++ src/test/java/com/CloudBuild/DemoCloudbuild/DemoCloudbuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CloudBuild.DemoCloudbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCloudbuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
